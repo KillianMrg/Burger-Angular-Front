be83646eaf31657352c31b93c7b84f7c5fb6d0be
KillianMrg <killian.morge@gmail.com>
2020-05-10 11:30:27 +0200

    Init material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiModule } from './api';
+import { AppComponent } from './app.component';
 import { BurgerListComponent } from './burger-list/burger-list.component';
 import { BurgerDetailsComponent } from './burger-details/burger-details.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { BurgerDetailsComponent } from './burger-details/burger-details.componen
     BrowserModule,
     AppRoutingModule,
     ApiModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
